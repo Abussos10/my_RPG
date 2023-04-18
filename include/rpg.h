ed31d319297f1e9a74b6e08a80bd59cd7e096cf3
ballandlucien <lucien.balland@epitech.eu>
2023-04-18 18:28:11 +0200

    [MOD] .h rearrangement with commentary

--- include/rpg.h
+++ include/rpg.h
@@ -32,33 +32,72 @@
 #define SETPOS sfSprite_setPosition
 #define KEYPRESSED sfKeyboard_isKeyPressed
 
+// 
+#define BUTTON_START
+#define BUTTON_RESUME
+#define BUTTON_SETTINGS
+#define BUTTON_QUIT
+
+// #define BUTTON_SETTINGS
+
+// window structure
 typedef struct window_s{
     sfVideoMode mode;
     sfEvent event;
     sfRenderWindow *window;
-}window_t;
+} window_t;
 
+// map structure
 typedef struct map_s{
     sfTexture *texture;
     sfSprite *sprite;
     sfVector2f scale;
     sfVector2f pos;
     float speed;
-}map_t;
+} map_t;
 
+// global structure
 typedef struct global_s{
     window_t settings;
     map_t **picture;
-}global_t;
+} global_t;
+
+
+
+// src/main.c :
+
+// src/game_process.c
+    int usage(int ac, char **av, global_t *all);
+    void game_process(global_t *all);
+
+// src/initialization/ :
+
+    // init_sprites.c
+    void init_scale(global_t *all);
+    void init_pos(global_t *all);
+    void init_setsprite(global_t *all);
+    void init_sprite(global_t *all);
+
+    // init_map.c
+    void init_window(global_t *all);
+    void eventclose(global_t *all);
+    void screenopen(global_t *all);
+
+// src/inventaire/ :
+
+    // inventaire.c
+
+// src/menu/ :
+
+    // menu.c
+    void open_main_menu(global_t *all);
+
+
+// src/perso/ :
+
+    // players_movements.c
+    int move_sprites(global_t *all);
+
 
 int my_strlen(char *str);
 int my_strcmp(char *base, char *acomp);
-void eventclose(global_t *ALL);
-void screenopen(global_t *ALL);
-void init_window(global_t *ALL);
-int usage(int ac, char **av, global_t *ALL);
-void init_scale(global_t *all);
-void init_pos(global_t *all);
-void init_setsprite(global_t *all);
-void init_sprite(global_t *all);
-int move_sprites(global_t *all);
