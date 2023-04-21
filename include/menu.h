76610e7e23075a978e6f63f337285d5968138b58
hotsoup <thomas.hurgargowitsch@epitech.eu>
2023-04-21 13:49:24 +0200

    [+] > Adding in .h menu_function

---
+++ include/menu.h
@@ -0,0 +1,49 @@
+/*
+** EPITECH PROJECT, 2023
+** my_rpg
+** File description:
+** menu
+*/
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <SFML/Graphics.h>
+#include <SFML/Window.h>
+#include <SFML/System.h>
+#include <SFML/System/Clock.h>
+#include <SFML/Graphics/Rect.h>
+#include <SFML/GPUPreference.h>
+#include <SFML/Audio.h>
+#include <stddef.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdbool.h>
+#include <stdarg.h>
+
+#pragma once
+
+typedef struct button_s {
+    sfSprite *quit_s;
+    sfTexture *quit_tx;
+    sfSprite *play_s;
+    sfTexture *play_tx;
+    sfSprite *set_s;
+    sfTexture *set_tx;
+    sfSprite *bquit_s;
+    sfTexture *bquit_tx;
+    sfSprite *bplay_s;
+    sfTexture *bplay_tx;
+    sfSprite *bset_s;
+    sfTexture *bset_tx;
+}button_t;
+
+typedef struct menu_s {
+    sfSprite *bg_s;
+    sfTexture *bg_tx;
+    sfMusic *menu_music;
+    sfSprite *tx_s;
+    sfTexture *tx_tx;
+    button_t *but;
+}menu_t;
--- include/rpg.h
+++ include/rpg.h
@@ -15,6 +15,7 @@
     #include <SFML/System/Clock.h>
     #include <SFML/Graphics/Rect.h>
     #include <SFML/GPUPreference.h>
+    #include <SFML/Audio.h>
     #include <stddef.h>
     #include <string.h>
     #include <sys/types.h>
@@ -22,6 +23,7 @@
     #include <fcntl.h>
     #include <stdbool.h>
     #include <stdarg.h>
+    #include "menu.h"
 
 #define sfCff sfTexture_createFromFile
 #define sfRWc sfRenderWindow_create
@@ -36,6 +38,7 @@ typedef struct window_s{
     sfVideoMode mode;
     sfEvent event;
     sfRenderWindow *window;
+    sfVector2i m_p;
 }window_t;
 
 typedef struct map_s{
@@ -62,3 +65,13 @@ void init_pos(global_t *all);
 void init_setsprite(global_t *all);
 void init_sprite(global_t *all);
 int move_sprites(global_t *all);
+int menu_loop(menu_t *menu, window_t *window, global_t *glob);
+void draw_all_menu(menu_t *menu, global_t *glob);
+void init_sprite_menu(menu_t *menu);
+void init_all_menu(menu_t *menu);
+int checkbutton_play(global_t *data, button_t *button);
+int checkbutton_quit(global_t *data, button_t *button);
+void init_button(button_t *but);
+int checkbutton_settings(global_t *data, button_t *button);
+void init_sprite_menu(menu_t *menu);
+
