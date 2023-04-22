e1b79e141a6e586f650deef49250b08aab36cd2b
ballandlucien <lucien.balland@epitech.eu>
2023-04-22 04:19:13 +0200

    [ADD] collision handler (the map is a bit shifted with the mask)

--- src/perso/players_collisions.c
+++ src/perso/players_collisions.c
@@ -6,19 +6,25 @@
 */
 
 #include "../../include/rpg.h"
-#include "../../include/menu.h"
 
-int collider(sfImage *collision -> "c'est la map en noire et blanc", récupérer position joueur)
+// function that check if the play is in front of a wall (1 true 0 false)
+int check_collision(global_t *all, int direction, int offsetx, int offsety)
 {
-    sfColor walk_character = sfImage_getPixel(collision,(unsigned int) positionx,(unsigned int)positiony);
-    sfUint32 wh = sfColor_toInteger(sfwhite);
-    sfUint32 bl = sfColor_toInteger(sfBlack);
-    sfUint32 wc = sfColor_toInteger(walk_character);
-    if (wc != bl)
-        return true;
-
-    else 
-        return false;
-
+    sfVector2f player_pos = GET_POS(all->player->sprt);
+    player_pos.x += (float)offsetx; player_pos.y += (float)offsety;
+    sfColor pixelColor = sfImage_getPixel(all->mask_image,\
+    player_pos.x / (float)5.0, player_pos.y / (float)4.9);
+    if (pixelColor.r == 255)
+        return (0);
+    switch (direction) {
+        case (LEFT):
+        sfSprite_move(all->player->sprt, (sfVector2f){1, 0}); break;
+        case (RIGHT):
+        sfSprite_move(all->player->sprt, (sfVector2f){-1, 0}); break;
+        case (BELOW):
+        sfSprite_move(all->player->sprt, (sfVector2f){0, -1}); break;
+        case (ABOVE):
+        sfSprite_move(all->player->sprt, (sfVector2f){0, 1}); break;
+    }
+    return (1);
 }
-appeler la fonction ou le personnage veut aller et non sous le personnage.
