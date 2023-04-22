fd2adbd31281366e860396181c8458de88a3fcce
ballandlucien <lucien.balland@epitech.eu>
2023-04-22 04:32:19 +0200

    [MOD] coding style

--- src/main.c
+++ src/main.c
@@ -14,9 +14,6 @@ int usage(int ac, char **av, global_t *all)
         write(1, "USAGE\n\t./my_rpg\n", 17);
         write(1, "\t my_rpg \tThe executable for the game\n\n", 40);
         write(1, "OPTIONS\n\t-h\t\tprint the usage and quit.\n\n", 41);
-        //write(1, "USER INTERACTIONS\n\t'L' key\t", 28);
-        //write(1, "enable/disable hitboxes and areas.\n", 36);
-        //write(1, "\t'S' key        enable/disable sprites.\n", 41);
     } else {
         init_window(all);
         init_sprite(all);
@@ -25,9 +22,8 @@ int usage(int ac, char **av, global_t *all)
         init_setsprite(all);
         init_player(all);
         all->clock->clk = sfClock_create();
-        //run();
     }
-    return 0;
+    return (0);
 }
 
 int main(int ac, char **av)
