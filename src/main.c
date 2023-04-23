070ae17acdba21a134e5d8a5932740148dbc79fd
ballandlucien <lucien.balland@epitech.eu>
2023-04-23 20:08:50 +0200

    [ADD] main

--- src/main.c
+++ src/main.c
@@ -16,7 +16,7 @@ int check_win_or_lose(int index, global_t *all, window_t *window)
     return (0);
 }
 
-int usage(int ac, char **av, global_t *all)
+int usage(global_t *all)
 {
     init_screen(all);
     init_window(all);
@@ -50,7 +50,7 @@ int main(void)
     all->player->npc = malloc(sizeof(npc_t));
     all->enemy = malloc(sizeof(enemy_t));
     all->end = malloc(sizeof(loose_win_t));
-    usage(ac, av, all);
+    usage(all);
     if (!(all->settings.window)) return 84;
     menu_loop(menu, window, all);
     initialize_all(all);
