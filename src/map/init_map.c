c9ff4d53351909d23f9a7ef41762dc1de48fe9f3
hotsoup <thomas.hurgargowitsch@epitech.eu>
2023-04-23 17:49:28 +0200

    [+] > Adding second enemi 2

--- src/map/init_map.c
+++ src/map/init_map.c
@@ -11,6 +11,7 @@
 void init_window(global_t *all)
 {
     init_value(all);
+    all->enemy->life_d = 100;
     MODE.bitsPerPixel = 64;
     MODE.width = 1920;
     MODE.height = 1080;
@@ -46,8 +47,6 @@ void screenopen(global_t *all)
         }
         game_events(all);
         if (LUH->health_status == 0) break;
-        if (all->enemy->life > 0)
-            fight(all), RENDER(all->settings.window, all->enemy->sprt, NULL);
         if (init_meeting_zone(all->player) == 1 && LUI->sword_status == 1)
             break;
         sfRenderWindow_display(all->settings.window);
@@ -66,12 +65,16 @@ void game_events(global_t *all)
     sword_event_handler(all);
     RENDER(all->settings.window, all->mask_iso->sprite, NULL);
     draw_npc(all->player->npc, all);
-    inventory_render(all); health_bar_render(all);
     if (init_meeting_zone(all->player) == 1 && LUI->sword_status == 0) {
         RENDER(all->settings.window,
         all->player->npc->b_sp, NULL);
         sfSound_play(all->music->sound);
     }
+    if (all->enemy->life_d > 0)
+        fight_d(all), RENDER(all->settings.window, all->enemy->sprt_d, NULL);
+    if (all->enemy->life > 0)
+        fight(all), RENDER(all->settings.window, all->enemy->sprt, NULL);
+    inventory_render(all); health_bar_render(all);
 }
 
 // function to initialize some useful value in my struct
